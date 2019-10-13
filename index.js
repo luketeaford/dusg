fa99083f979ebc654704f3910540431be403a9b5
Luke Teaford <luketeaford@gmail.com>
2019-10-13 19:31:42 -0400

    Add smaller tests in case of regressions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const compose = (f, g) => x => f(g(x))
 const index = async function (aFilePath, aDestDir, aTemplateFn) {
   const data = await fs.readFile(aFilePath)
   const template = compose(aTemplateFn, parse)
-  await fs.mkdir(`${aDestDir}`)
+  await fs.mkdir(`${aDestDir}`, { recursive: true })
   return fs.writeFile(`${aDestDir}/example.html`, template(data))
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,33 @@ const fs = require('fs').promises
 
 const mockTemplateFn = pageData => `<title>${pageData.metadata.title}</title>`
 
-test('The index function creates an html file in the chosen directory with the contents from the source file parsed as markdown and yaml and rendered by the supplied template function.', async t => {
+test('The index function creates an html file from the source file.', async t => {
+  await index('./test/data/example.md', '.', x => x)
+    .catch(err => t.end(err))
+
+  fs.readFile('./example.html')
+    .then(async data => {
+      t.equal(data.length > 0, true)
+      await fs.unlink('./example.html')
+      t.end()
+    })
+    .catch(err => t.end(err))
+})
+
+test('The index function uses the template function the user supplies to create an html file from the source file parsed as Markdown and YAML.', async t => {
+  await index('./test/data/example.md', '.', mockTemplateFn)
+    .catch(err => t.end(err))
+
+  fs.readFile('./example.html')
+    .then(async data => {
+      t.equal(data.toString(), '<title>Page Title</title>')
+      await fs.unlink('./example.html')
+      t.end()
+    })
+    .catch(err => t.end(err))
+})
+
+test('The index function creates an html file in the chosen directory with the contents from the source file.', async t => {
   await index('./test/data/example.md', './test-public', mockTemplateFn)
     .catch(err => t.end(err))
 
