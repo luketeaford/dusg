b295094069f7f573791d228aa8cd44bac56abc96
Luke Teaford <luketeaford@gmail.com>
2019-12-24 16:51:44 -0500

    Remove hardcoded filename

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const parse = require('./lib/parse')
 
 const compose = (f, g) => x => f(g(x))
 
+const getFilename = x => x.slice(x.lastIndexOf('/'))
+
 const index = async function (aFilePath, aDestDir, aTemplateFn) {
   const data = await fs.readFile(aFilePath)
   const template = compose(aTemplateFn, parse)
   await fs.mkdir(`${aDestDir}`, { recursive: true })
-  return fs.writeFile(`${aDestDir}/example.html`, template(data))
+  const filename = getFilename(aFilePath).replace('.md', '.html')
+  return fs.writeFile(`${aDestDir}${filename}`, template(data))
 }
 
 module.exports = index
