2bfc371cc152f6e750e74ac3b24976dae8e19a08
Luke Teaford <luketeaford@gmail.com>
2019-12-25 16:51:04 -0500

    Support  any output directory

--- test/build.test.js
+++ test/build.test.js
@@ -3,14 +3,14 @@ const build = require('../lib/build')
 const fs = require('fs').promises
 
 test('The build function', async t => {
-  await build('./test/data')
+  await build('./test/data', './test-generated')
     .catch(err => t.end(err))
 
-  fs.readFile('./test-output/very/very/very/deep/nesting/stinks.txt')
+  fs.readFile('./test-generated/very/very/very/deep/nesting/stinks.txt')
     .then(async data => {
       t.equal(data.toString().trim(), 'Something fishy', 'copies files a little.')
-      fs.unlink('./test-output/very/very/very/deep/nesting/stinks.txt')
-      await fs.rmdir('./test-output', { recursive: true })
+      fs.unlink('./test-generated/very/very/very/deep/nesting/stinks.txt')
+      await fs.rmdir('./test-generated', { recursive: true })
         .catch(err => t.end(err))
       t.end()
     })
