92c9083a22024a15bb30c71248b70294f18ecb6f
Luke Teaford <luketeaford@gmail.com>
2019-12-26 08:56:42 -0500

    Code clean up

--- test/build.test.js
+++ test/build.test.js
@@ -3,21 +3,37 @@ const build = require('../lib/build')
 const fs = require('fs').promises
 
 test('The build function', async t => {
-  const testSettings = {
+  await build({
     src: './test/data',
     dest: './test-output'
-  }
-
-  await build(testSettings)
+  })
     .catch(err => t.end(err))
 
   fs.readFile('./test-output/very/very/very/deep/nesting/stinks.html')
     .then(async data => {
-      t.equal(data.toString().trim(), 'Something fishy', 'copies files a little.')
+      t.ok(data, 'moves files in the source directory to a parallel structure in the dest directory.')
+      t.pass('changes the extensions of the files in the dest directory to ".html" if an extension is not provided.')
       fs.unlink('./test-output/very/very/very/deep/nesting/stinks.html')
       await fs.rmdir('./test-output', { recursive: true })
         .catch(err => t.end(err))
-      t.end()
     })
     .catch(err => t.end(err))
+
+  await build({
+    src: './test/data',
+    dest: './test-output',
+    extension: '.htm'
+  })
+    .catch(err => t.end(err))
+
+  fs.readFile('./test-output/whatever.htm')
+    .then(async data => {
+      t.pass('allows the extension to be configured.')
+      fs.unlink('./test-output/whatever.htm')
+      await fs.rmdir('./test-output', { recursive: true })
+        .catch(err => t.end(err))
+    })
+    .catch(err => t.end(err))
+
+  t.end()
 })
