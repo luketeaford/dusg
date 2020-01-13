ca69252ffa2960d1ef828b76a614af4475f16553
Luke Teaford <luketeaford@gmail.com>
2020-01-13 18:00:08 -0500

    Add to CLI

--- bin/dusg.js
+++ bin/dusg.js
@@ -2,8 +2,22 @@
 
 const manchego = require('manchego').convertBooleans
 const build = require('../lib/build')
+const { version } = require('../package.json')
 
 const cliSettings = manchego(process.argv)
-const template = require(`${process.cwd()}/${cliSettings.template}`)
 
-build({ ...cliSettings, template })
+if (cliSettings.help || cliSettings.h) {
+  const fs = require('fs').promises
+  fs.readFile(`${__dirname}/../man/dusg.1`, { encoding: 'utf-8' })
+    .then(console.log)
+} else if (cliSettings.version || cliSettings.v) {
+  console.log(version)
+} else {
+  const template = require(`${process.cwd()}/${cliSettings.template}`)
+
+  build({
+    src: cliSettings.src || cliSettings.s,
+    dest: cliSettings.dest || cliSettings.d,
+    template
+  })
+}
